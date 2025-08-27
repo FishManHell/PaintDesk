d18b9f12a1c3035e8be13259f5492bf1c5d58651
Denys <denys.z@intell-act.com>
2025-08-27 22:58:19 +0300

    add webpack, typescript, styles, folders, install libraries

---
+++ config/webpack/buildPlugins.ts
@@ -0,0 +1,36 @@
+import {BuildOptions} from "./types";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import {WebpackPluginInstance, HotModuleReplacementPlugin, ProgressPlugin, DefinePlugin} from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export const buildPlugins = (options: BuildOptions): WebpackPluginInstance[] => {
+    const {isDev, project, paths: {html: template}} = options
+
+    const definePluginVariables: Record<string, string> = {
+        __IS_DEV__: JSON.stringify(isDev),
+        __PROJECT__: JSON.stringify(project)
+    }
+
+    const devPlugins = [
+        new ReactRefreshWebpackPlugin({
+            overlay: false, // leave it here for now
+        }),
+        new HotModuleReplacementPlugin(),
+    ]
+
+    const plugins = [
+        new HtmlWebpackPlugin({template}),
+        new ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+
+        new DefinePlugin(definePluginVariables),
+    ]
+
+    if (isDev) plugins.push(...devPlugins);
+
+    return plugins
+}
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import {App} from "app/App";
+import "./app/styles/index.scss"
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
+    <App/>
+)
+

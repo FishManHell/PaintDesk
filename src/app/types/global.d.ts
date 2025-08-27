846c2d41b7e3f1364ca6cc67ca64767530da338c
Denys <denys.z@intell-act.com>
2025-08-27 23:04:16 +0300

    install libraries, add global.d.ts file

---
+++ src/app/types/global.d.ts
@@ -0,0 +1,20 @@
+declare module '*.scss' {
+    interface IClassNames {
+        [className: string]: string
+    }
+    const classNames: IClassNames;
+    export = classNames;
+}
+
+declare module '*.svg';
+declare module '*.jpg';
+declare module '*.jpeg';
+
+declare module "*.svg" {
+    import React from "react";
+    const SVG: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
+    export default SVG;
+}
+
+declare const __IS_DEV__: boolean;
+declare const __PROJECT__: string
